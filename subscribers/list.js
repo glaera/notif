7b080248fb8094a9a1d657a246cee59841667f19
glaera <31164368+glaera@users.noreply.github.com>
2017-12-19 08:30:40 +0100

    Update list.js

--- subscribers/list.js
+++ subscribers/list.js
@@ -56,7 +56,6 @@ var listHandler = function(req,res){
     console.log('gennaro USER',req.user);
     var oauth2Client = getOAuthClient();
     oauth2Client.credentials = req.session["tokens"];
-console.log('GENNARO???',req.session);
 /*
     var oauth2 = google.oauth2({
       auth: oauth2Client,
@@ -74,11 +73,9 @@ console.log('GENNARO???',req.session);
 */
     getModel().list(10, null, (err, entities, cursor) => {
       if (err) {
-        console.log('gennaro ',err)
         res.end("error");;
         return;
       }
-      console.log('gennaro reducing to ',asHtml(entities))
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(asHtml(entities));
       if (false)res.json({
